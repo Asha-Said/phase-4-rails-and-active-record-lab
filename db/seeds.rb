05b92ee2694716b9796684b72f8fa9f56ae0f1ae
aisha-said <aisha-said@github.com>
2022-11-28 12:52:54 +0300

    finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create(first_name: "Dwayne", second_name: "Johnson", grade: 5)
